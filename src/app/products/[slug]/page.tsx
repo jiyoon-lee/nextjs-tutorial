4573bab92a07ad42666cdd9d33752679ac885341
jiyoon-lee <jiyoon3421@gmail.com>
2023-06-25 20:26:47 +0900

    4.19 SEO 중요 내용

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -7,6 +7,11 @@ type Props = {
   };
 };
 
+export function generateMetadata({ params }: Props) {
+  return {
+    title: `제품의 이름: ${params.slug}`,
+  };
+}
 const PantsPage = ({ params }: Props) => {
   if (params.slug === "nothing") {
     notFound();
--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import styles from "./layout.module.css";
 import Link from "next/link";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "멋진 제품 사이트 | 전체 제품 확인",
+  description: "멋진 제품을 확인해 보세요.",
+};
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
